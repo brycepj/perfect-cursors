ef1efa22827186975900df360744154bcf84fc9e
Bryce Johnson <brycejohnson@slack-corp.com>
2022-02-09 17:00:25 -0700

    Remove dep from tldraw.

--- perfect-cursors/src/perfect-cursor.ts
+++ perfect-cursors/src/perfect-cursor.ts
@@ -1,4 +1,4 @@
-import { Vec } from '@tldraw/vec'
+import { Vec } from './vec'
 import { Spline } from './spline'
 
 type AnimationState = 'stopped' | 'idle' | 'animating'
--- perfect-cursors/src/spline.ts
+++ perfect-cursors/src/spline.ts
@@ -1,4 +1,4 @@
-import { Vec } from '@tldraw/vec'
+import { Vec } from './vec'
 
 export class Spline {
   points: number[][] = []
